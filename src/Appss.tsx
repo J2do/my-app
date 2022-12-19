9ec7e85b1f0bb86c503ca7048769f32ad4299427
vlad <jun2damoon@gmail.com>
2022-12-19 21:33:19 +0300

    новый коммит

---
+++ src/Appss.tsx
@@ -0,0 +1,33 @@
+// import React from "react";
+// import logo from "./logo.svg";
+// import "./App.css";
+// import { Button } from "antd";
+
+// // function App() {
+// //   return (
+// //     <div className="App">
+// //       <header className="App-header">
+// //         <img src={logo} className="App-logo" alt="logo" />
+// //         <p>
+// //           Edit <code>src/App.tsx</code> and save to reload.
+// //         </p>
+
+// //         <Button size="large" loading={true}>
+// //           Click me
+// //         </Button>
+// //       </header>
+// //     </div>
+// //   );
+// // }
+
+// function App() {
+//   return(
+//     <div>
+//       <header>
+//         <
+//       </header>
+//     </div>
+
+// )}
+
+// export default App;
