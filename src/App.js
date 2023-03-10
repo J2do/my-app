8e56d7b7fed651172d65d2a29656e929758a32ce
j2do <jun2damoon@gmail.com>
2023-03-10 17:52:46 +0300

    useMemo

--- src/App.js
+++ src/App.js
@@ -1,30 +1,34 @@
-import React, { useState } from "react";
+import React, { useMemo, useState } from "react";
 import PostList from "./components/PostList";
 import "./styles/App.css";
 import PostForm from "./components/PostForm";
-import MySelect from "./components/UI/select/MySelect";
-import MyInput from "./components/UI/input/MyInput";
+import PostFilter from "./components/PostFilter";
 
 function App() {
   const [posts, setPosts] = useState([
-    { id: 1, title: "JavaScript 1", body: "Description" },
-    { id: 2, title: "JavaScript 2", body: "Description" },
-    { id: 3, title: "JavaScript 3", body: "Description" },
+    { id: 1, title: "aaa", body: "zzz" },
+    { id: 2, title: "ddd", body: "yyy" },
+    { id: 3, title: "vvv", body: "www" },
   ]);
 
-  const [selectedSort, setSelectedSort] = useState("");
-  const [searchQuery, setSearchQuery] = useState("");
+  const [filter, setFilter] = useState({ sort: "", query: "" });
 
-  function getSortedPosts() {
+  const sortedPosts = useMemo(() => {
     console.log("work");
-    if (selectedSort) {
-      return [...posts].sort((a, b) =>
-        a[selectedSort].localeCompare(b[selectedSort])
+    if (filter.sort) {
+      return [...posts].sort((a, b, c) =>
+        a[filter.sort].localeCompare(b[filter.sort])
       );
-    } else return posts;
-  }
+    }
+    return posts;
+  }, [filter.sort, posts]);
 
-  const sortedPosts = getSortedPosts;
+  const sortedAndSearchPosts = useMemo(() => {
+    return sortedPosts.filter((post) =>
+      post.title.toLowerCase().includes(filter.query)
+    );
+  }, [filter.query, sortedPosts]);
+
   const createPost = (newPost) => {
     setPosts([...posts, newPost]);
   };
@@ -33,34 +37,16 @@ function App() {
     setPosts(posts.filter((p) => p.id !== post.id));
   };
 
-  const sortPosts = (sort) => {
-    setSelectedSort(sort);
-  };
-
   return (
     <div className="App">
       <PostForm create={createPost} />
-      <div>
-        <MyInput
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-          placeholder="Search..."
-        />
-        <MySelect
-          value={selectedSort}
-          onChange={sortPosts}
-          defaultValue="sort"
-          option={[
-            { value: "title", name: "massage" },
-            { value: "tag", name: "#" },
-          ]}
-        />
-      </div>
-      {posts.length !== 0 ? (
-        <PostList remove={removePost} posts={sortedPosts} title="POST R" />
-      ) : (
-        <h1 style={{ textAlign: "center" }}>Not Find!</h1>
-      )}
+      <hr style={{ margin: "15px 0" }} />
+      <PostFilter filter={filter} setFilter={setFilter} />
+      <PostList
+        remove={removePost}
+        posts={sortedAndSearchPosts}
+        title="POST R"
+      />
     </div>
   );
 }
--- src/components/UI/select/MySelect.jsx
+++ src/components/UI/select/MySelect.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 
-const MySelect = ({ option, defaultValue, value, onChange }) => {
+const MySelect = ({ options, defaultValue, value, onChange }) => {
   return (
     <select value={value} onChange={(event) => onChange(event.target.value)}>
       <option disabled value="">
         {defaultValue}
       </option>
-      {option.map((option) => (
+      {options.map((option) => (
         <option key={option.value} value={option.value}>
           {option.name}
         </option>
